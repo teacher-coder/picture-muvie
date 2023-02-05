920122725b394406b70ada766ea40e026f6e44a2
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-05 18:06:18 +0900

    feat: frontend initail commit

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap p-6">
+    <div class="flex items-center flex-shrink-0 mr-6">
+      <IconMovie />
+      <span class="font-semibold text-xl tracking-tight ml-6"
+        >그림 뮤비 제작</span
+      >
+    </div>
+
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a class="block mt-4 lg:inline-block lg:mt-0 hover:text-red-500 mr-4">
+          사용방법
+        </a>
+        <a class="block mt-4 lg:inline-block lg:mt-0 hover:text-red-500 mr-4">
+          건의하기
+        </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+          >Download</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import IconMovie from '@/icons/IconMovie.vue'
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-6 mx-16 h-[70vh]">
+    <Form class="flex flex-col">
+      <label for="lyrics" class="text-xl font-bold mb-3">가사 찾기</label>
+      <div>
+        <Field name="lyrics" type="text" class="border-2 border-solid" />
+        <button
+          class="bg-rose-600 text-white font-bold py-1 px-4 ml-4 rounded-md"
+        >
+          검색
+        </button>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Form, Field } from 'vee-validate'
+</script>
