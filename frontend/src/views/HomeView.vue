90900e88c1689f9d247d7d2c7bc92d38a4fbde02
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-07 16:45:41 +0900

    feat: axios setting & get request for lyrics

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="my-6 mx-16 h-[70vh]">
-    <Form class="flex flex-col">
-      <label for="lyrics" class="text-xl font-bold mb-3">가사 찾기</label>
+    <form class="flex flex-col" @submit.prevent="searchLyrics">
+      <label for="search" class="text-xl font-bold mb-3">가사 찾기</label>
       <div>
-        <Field name="lyrics" type="text" class="border-2 border-solid" />
+        <input
+          name="search"
+          type="text"
+          v-model="searchTerm"
+          class="border-2 border-solid"
+        />
         <button
           class="bg-rose-600 text-white font-bold py-1 px-4 ml-4 rounded-md"
         >
           검색
         </button>
       </div>
-    </Form>
+    </form>
   </div>
 </template>
 
 <script setup>
-import { Form, Field } from 'vee-validate'
+import { ref } from 'vue'
+import api from '@/api/modules/lyrics'
+
+const searchTerm = ref('')
+async function searchLyrics() {
+  console.log(searchTerm.value)
+  const lyrics = await api.getLyrics({ params: { search: searchTerm.value } })
+  console.log(lyrics)
+}
 </script>
