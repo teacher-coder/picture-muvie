bc9c530b321b1149e0f6bbced930d3304f82bebd
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-07 18:09:43 +0900

    feat: text area for splitting lyrics

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,32 +1,65 @@
 <template>
-  <div class="my-6 mx-16 h-[70vh]">
-    <form class="flex flex-col" @submit.prevent="searchLyrics">
-      <label for="search" class="text-xl font-bold mb-3">가사 찾기</label>
-      <div>
+  <div class="my-6 mx-7 h-[70vh] flex flex-col w-full space-y-5">
+    <form class="flex flex-col space-y-3" @submit.prevent="sendSongData">
+      <div class="text-xl font-bold">가사 찾기</div>
+      <div class="flex flex-col">
+        <label for="song" class="text-lg">노래 제목</label>
         <input
-          name="search"
+          name="song"
           type="text"
-          v-model="searchTerm"
+          v-model="songName"
           class="border-2 border-solid"
         />
-        <button
-          class="bg-rose-600 text-white font-bold py-1 px-4 ml-4 rounded-md"
-        >
-          검색
-        </button>
       </div>
+      <div class="flex flex-col">
+        <label for="artist" class="text-lg">가수 이름</label>
+        <input
+          name="artist"
+          type="text"
+          v-model="artistName"
+          class="border-2 border-solid"
+        />
+      </div>
+      <button class="bg-rose-600 text-white font-bold py-1 px-4 rounded-md">
+        검색
+      </button>
+    </form>
+    <form class="flex flex-col space-y-3" @submit.prevent="sendLyricsData">
+      <label for="split" class="text-xl font-bold">가사 구간 나누기</label>
+      <textarea
+        name="split"
+        type="text"
+        v-model="lyrics_text"
+        rows="8"
+        class="p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+      <div class="text-right text-lg">
+        학급 인원 : {{ lyrics_list.split('\n').filter((n) => n).length }}명
+      </div>
+      <button class="bg-rose-600 text-white font-bold py-1 px-4 rounded-md">
+        다운로드
+      </button>
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import api from '@/api/modules/lyrics'
+import { computed, ref } from 'vue'
 
-const searchTerm = ref('')
-async function searchLyrics() {
-  console.log(searchTerm.value)
-  const lyrics = await api.getLyrics({ params: { search: searchTerm.value } })
-  console.log(lyrics)
+const songName = ref('')
+const artistName = ref('')
+const lyrics_text = ref('')
+const lyrics_list = computed(() => lyrics_text.value)
+
+async function sendSongData() {
+  const lyrics = await api.getLyrics({
+    params: { search: `${songName.value} ${artistName.value}` },
+  })
+  lyrics_text.value = lyrics
+}
+
+async function sendLyricsData() {
+  console.log(lyrics_list.value)
 }
 </script>
