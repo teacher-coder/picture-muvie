ffc683335dce9f0092bde9d26a204f7d1cd923ab
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-07 19:07:31 +0900

    feat: remove items from footer

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -27,10 +27,6 @@ const navigation = {
   main: [
     { name: 'About', href: '#' },
     { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Accessibility', href: '#' },
-    { name: 'Partners', href: '#' },
   ],
   social: [
     {
