7b9fe66652710dd3bd6e6b653fb449775c2b0775
Ridealist <cjsn92@gmail.com>
2023-02-08 23:06:54 +0900

    refactor: create backend directory for django api server

--- backend/picture_muvie/settings.py
+++ backend/picture_muvie/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "picture_muvie.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
