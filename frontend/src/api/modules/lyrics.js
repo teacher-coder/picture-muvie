48dd17b4922993062757df86e9b8378aaa4fc819
Ridealist <cjsn92@gmail.com>
2023-02-10 13:38:47 +0900

    refactor: add file specification setting & apply frontend logic

--- frontend/src/api/modules/lyrics.js
+++ frontend/src/api/modules/lyrics.js
@@ -7,7 +7,7 @@ export default {
       .get(urls.getLyrics, ...params)
       .then((response) => response.data)
   },
-  async downloadLyricsDocx(...params) {
+  async downloadLyrics(...params) {
     return myAxios
       .post(urls.downloadLyricsDocx, ...params, {
         responseType: 'blob',
@@ -15,6 +15,6 @@ export default {
           'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
         }
       })
-      .then((response) => response.data)
+      .then((response) => response)
   },
 }
