41bf437c2f5eff08508c9df8e1b9a89e3cffb9c6
Ridealist <cjsn92@gmail.com>
2023-02-12 16:19:53 +0900

    Revert "refactor: recreate make docx file algorithm & remove template files"
    This reverts commit d8844aae0fbfe58793ec2e4bf0544002d9314e16.

--- backend/picture_muvie/docs.py
+++ backend/picture_muvie/docs.py
@@ -1,104 +1,53 @@
 from docx import Document
-from docx.enum.section import WD_ORIENTATION
-from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_BREAK
-from docx.text.paragraph import Paragraph
-from docx.shared import Mm, Pt
+from docxcompose.composer import Composer
+from docxtpl import DocxTemplate, RichText
 
 
-def make_a4_landscape_section() -> Document:
-    doc = Document()
-    sections = doc.sections
-    section = sections[0]
-    # a4 size in 'mm' unit
-    section.page_height = Mm(297)
-    section.page_width = Mm(210)
-    # portrait -> landscape
-    section.page_width, section.page_height = section.page_height, section.page_width
-    section.orientation = WD_ORIENTATION.LANDSCAPE
-    # margin setting
-    section.top_margin = Mm(15)
-    section.bottom_margin = Mm(15)
-    section.left_margin = Mm(15)
-    section.right_margin = Mm(15)
+def make_header(title: str) -> Document:
+    doc = DocxTemplate("templates/lyrics_header_form.docx")
+
+    context = {
+        "title": title,
+    }
+    doc.render(context)
+    return doc
+
+
+def make_body(line: str) -> Document:
+    doc = DocxTemplate("templates/lyrics_body_form.docx")
+
+    rt = reform_font_size(line)
+    context = {
+        "lyrics": rt,
+    }
+    doc.render(context)
 
     return doc
 
-def isHangeul(ch) -> bool:
-    JAMO_START_LETTER = 44032
-    JAMO_END_LETTER = 55203
-    return ord(ch) >= JAMO_START_LETTER and ord(ch) <= JAMO_END_LETTER
 
-def isAlphabet(ch) -> bool:
-    UPPER_A_LETTER = 65
-    UPPER_Z_LETTER = 90
-    LOWER_A_LETTER = 97
-    LOWER_Z_LETTER = 122
-    return (ord(ch) >= UPPER_A_LETTER and ord(ch) <= UPPER_Z_LETTER) or (ord(ch) >= LOWER_A_LETTER and ord(ch) <= LOWER_Z_LETTER)
+def make_doc(title: str, lyrics: list[str]) -> Document:
+    master = make_header(title)
+    composer = Composer(master)
 
-def isSpace(ch) -> bool:
-    return ord(ch) == 32
+    for lyric in lyrics:
+        doc2 = make_body(lyric)
+        composer.append(doc2)
 
-def count_lyric_language(lyric : str) -> dict:
-    chr_dict = {'alphabet':0, 'hangeul':0, 'space':0, 'special':0}
-    for c in lyric:
-        if isAlphabet(c):
-            chr_dict['alphabet'] += 1
-        elif isHangeul(c):
-            chr_dict['hangeul'] += 1
-        elif isSpace(c):
-            chr_dict['space'] += 1
-        else:
-            chr_dict['special'] += 1
-    
-    return chr_dict
+    return master
 
 
-def reform_font_size(paragraph : Paragraph, lyric : str):
-    run = paragraph.add_run(lyric)
-    run.bold = True
-    font = run.font
-    font.name = 'malgun gothic'
+def reform_font_size(lyric: str):
+    rt = RichText()
+    lyric.strip()
 
-    chr_dict = count_lyric_language(lyric)
-
-    lyric_length = int(chr_dict['alphabet'] * 0.4 + chr_dict['hangeul'] + chr_dict['space'] * 0.7 + chr_dict['special'] * 0.3)
-
-    length = lyric_length
-    if length > 25:
-        font.size = Pt(20)
+    length = len(lyric)
+    if length > 30:
+        rt.add(lyric, size=50)
     elif length > 20:
-        font.size = Pt(25)
-    elif length > 15:
-        font.size = Pt(30)
-    elif length > 10:
-        font.size = Pt(40)
+        rt.add(lyric, size=65)
+        rt.add("\n", size=10)
     else:
-        font.size = Pt(50)
+        rt.add(lyric, size=75)
+        rt.add("\n", size=10)
 
-
-def make_doc_title(doc : Document, title : str):
-    paragraph = doc.add_paragraph()
-    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-    run = paragraph.add_run()
-    run.bold = True
-    font = run.font
-    font.name = 'malgun gothic'
-    font.size = Pt(60)
-    font.all_caps = True
-    run.add_text(title)
-
-
-def make_doc(title : str, lyrics : list[str]) -> Document:
-    doc = make_a4_landscape_section()
-    make_doc_title(doc, title)
-
-    for i in range(len(lyrics)):
-        doc.add_section()
-        doc.sections[i + 1].footer.is_linked_to_previous = False
-
-        paragraph = doc.sections[i + 1].footer.paragraphs[0]
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-        reform_font_size(paragraph, lyrics[i])
-    return doc
+    return rt
