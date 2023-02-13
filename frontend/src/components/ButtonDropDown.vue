0d534f01fe07795ff51d4cf55216bac8c4942258
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-14 06:30:33 +0900

    feat: Float component to control position

--- frontend/src/components/ButtonDropDown.vue
+++ frontend/src/components/ButtonDropDown.vue
@@ -1,49 +1,48 @@
 <template>
-  <Menu as="div" class="relative inline-block text-left">
-    <MenuButton
-      class="inline-flex rounded-md border border-transparent bg-secondary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:text-sm"
+  <Menu>
+    <Float
+      placement="right-end"
+      :offset="10"
+      enter="transition duration-200 ease-out"
+      enter-from="scale-95 opacity-0"
+      enter-to="scale-100 opacity-100"
+      leave="transition duration-150 ease-in"
+      leave-from="scale-100 opacity-100"
+      leave-to="scale-95 opacity-0"
+      tailwindcss-origin-class
     >
-      {{ name }}
-      <ChevronDownIcon
-        class="ml-2 -mr-1 h-5 w-5 text-white"
-        aria-hidden="true"
-      />
-    </MenuButton>
+      <MenuButton class="bg-rose-600 text-white font-bold py-2">
+        <span class="inline-flex">
+          {{ name }}
+          <ChevronDownIcon
+            class="ml-2 h-5 w-5 text-white -rotate-90"
+            aria-hidden="true"
+        /></span>
+      </MenuButton>
 
-    <transition
-      enter-active-class="transition duration-100 ease-out"
-      enter-from-class="transform scale-95 opacity-0"
-      enter-to-class="transform scale-100 opacity-100"
-      leave-active-class="transition duration-75 ease-in"
-      leave-from-class="transform scale-100 opacity-100"
-      leave-to-class="transform scale-95 opacity-0"
-    >
-      <MenuItems
-        class="absolute right-0 mt-2 w-28 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-      >
-        <div class="px-1 py-1">
-          <MenuItem
-            v-slot="{ active }"
-            v-for="(item, index) in items"
-            :key="index"
+      <MenuItems class="space-y-3 mb-3">
+        <MenuItem
+          v-slot="{ active }"
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <button
+            :class="[
+              active ? ' bg-rose-800 text-white' : 'bg-rose-600 text-white',
+              'flex w-full items-center px-10 py-2 text-sm',
+            ]"
+            @click="item.onClicked"
           >
-            <button
-              :class="[
-                active ? 'bg-secondary text-white' : 'text-gray-900',
-                'flex w-full items-center rounded-md px-2 py-2 text-sm',
-              ]"
-              @click="item.onClicked"
-            >
-              {{ item.name }}
-            </button>
-          </MenuItem>
-        </div>
+            {{ item.name }}
+          </button>
+        </MenuItem>
       </MenuItems>
-    </transition>
+    </Float>
   </Menu>
 </template>
 
 <script setup>
+import { Float } from '@headlessui-float/vue'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 
