c1c18864adecf5098f298d1677715b5a3c092b42
hoetaek <hoetaek@student.snue.ac.kr>
2023-02-22 20:31:25 +0900

    refactor: import order

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import App from '@/App.vue'
+import '@/assets/main.css'
 import router from '@/router'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import '@/assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.provide('axios', app.config.globalProperties.axios)
+
 app.mount('#app')
