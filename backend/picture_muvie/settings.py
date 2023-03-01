33e7f551db6ad54327c8d9a8e5f9b657f04dd3b0
Ridealist <cjsn92@gmail.com>
2023-03-01 18:59:56 +0900

    feat: delete static file url and root_folder settings

--- backend/picture_muvie/settings.py
+++ backend/picture_muvie/settings.py
@@ -130,9 +130,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# STATIC_ROOT is relative to manage.py, which is in our top-level backend folder: /backend/static/
-STATIC_ROOT = "static"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
